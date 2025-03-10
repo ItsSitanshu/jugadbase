f8137ef89f5fb4c36ac3d155ffa4f5332172a034
ItsSitanshu <sitanshu15shrestha@gmail.com>
2025-03-10 20:07:30 +0545

    Try better looking docs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Josefin_Sans, Comfortaa } from "next/font/google";
+import { Josefin_Sans, Comfortaa, Fira_Code } from "next/font/google";
 import "./globals.css";
 
 const JosefinSans = Josefin_Sans({
@@ -9,6 +9,11 @@ const JosefinSans = Josefin_Sans({
 
 const ComfortaaFont = Comfortaa({
   variable: "--font-comforta",
+  subsets: ["latin"],
+});
+
+const FiraCodeFont = Fira_Code({
+  variable: "--font-fira",
   subsets: ["latin"],
 });
 
@@ -28,7 +33,7 @@ export default function RootLayout({
         <link rel="icon" type="image/svg+xml" href="/brand_logo.svg" />
       </head>
       <body
-        className={`${JosefinSans.variable} ${ComfortaaFont.variable} antialiased`}
+        className={`${JosefinSans.variable} ${ComfortaaFont.variable} ${FiraCodeFont.variable} antialiased`}
       >
         {children}
       </body>
--- app/ui/DocsComponent.tsx
+++ app/ui/DocsComponent.tsx
@@ -1,15 +1,18 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { unified } from 'unified';
+
 import ReactMarkdown from 'react-markdown';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import remarkParse from 'remark-parse';
-import { unified } from 'unified';
 import rehypePrism from 'rehype-prism-plus';
 import 'prismjs/themes/prism-twilight.css';
 
 import Navbar from '@/app/ui/Navbar';
+
+import './DocsMarkdown.css';
 
 interface Heading {
   level: number;
@@ -74,15 +77,55 @@ const DocsComponent = ({ page, subpage, subsubpage }: { page?: string; subpage?:
   };
 
   return (
-    <>
+    <div className="min-h-screen min-w-screen bg-[var(--background)]">
       <Navbar />
-      <div className="doc-wrapper">
-        <div className="content-wrapper">
-          <div className="markdown-content">
-            <ReactMarkdown children={content} rehypePlugins={[rehypeSlug, rehypeAutolinkHeadings, rehypePrism]} />
-          </div>
+      <div className="flex flex-row w-full h-full bg-[var(--background)] justify-center mt-5">
+        <div className="h-full w-1/2 bg-[var(--background)] text-[var(--foreground)]" >
+          <ReactMarkdown 
+            children={content}
+            rehypePlugins={[rehypeSlug, rehypeAutolinkHeadings, rehypePrism]} 
+            components={{
+              h1: ({ node, ...props }) => (
+                <div className="relative w-full">
+                  <h1 className="absolute left-[-1.8rem] top-[0.2rem] text-3xl text-white/20 font-fira">#</h1>
+                  <h1 className="text-4xl font-bold border-[var(--secondary)] mb-2 font-fira" {...props} />
+                </div>
+
+              ),
+              h2: ({ node, ...props }) => (
+                <h2 className="text-2xl py-1 mb-2 hover:text-[var(--secondary)] transition-all duration-300 font-fira" {...props} />
+              ),
+              p: ({ node, ...props }) => (
+                <p className="leading-relaxed font-fira" {...props} />
+              ),
+              strong: ({ node, ...props}) => (
+                <strong className='font-fira' {...props}/>
+              ),
+              a: ({ node, ...props }) => (
+                <a className="text-[var(--tertiary)] underline hover:text-secondary transition-colors font-fira" {...props} />
+              ),
+              ul: ({ node, ...props }) => (
+                <ul className="list-['-_'] pl-5 space-y-2 font-fira" {...props} />
+              ),
+              ol: ({ node, ...props }) => (
+                <ol className="list-[upper-roman] pl-5 font-fira" {...props} />
+              ),
+              li: ({ node, ...props }) => (
+                <li className="font-fira " {...props} />
+              ),
+              blockquote: ({ node, ...props }) => (
+                <blockquote className="border-l-4 border-tertiary pl-4 italic font-fira" {...props} />
+              ),
+              code: ({ node, ...props }) => (
+                <code className="rounded px-1 bg-light-background font-fira" {...props} />
+              ),
+              pre: ({ node, ...props }) => (
+                <pre className="p-3 rounded overflow-x-auto bg-dark-background font-fira" {...props} />
+              ),
+            }}
+          />
         </div>
-        {page && (
+        {/* {page && (
           <aside className="toc">
             <p>On this document</p>
             <ul>
@@ -93,9 +136,9 @@ const DocsComponent = ({ page, subpage, subsubpage }: { page?: string; subpage?:
               ))}
             </ul>
           </aside>
-        )}
+        )} */}
       </div>
-    </>
+    </div>
   );
 };
 
