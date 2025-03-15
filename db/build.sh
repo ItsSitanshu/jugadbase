22325a7293443dcfcff294476780a8252fc51e9a
ItsSitanshu <sitanshu15shrestha@gmail.com>
2025-03-15 16:07:48 +0545

    Implement multiple flags for build script

--- db/build.sh
+++ db/build.sh
@@ -1,2 +1,45 @@
-cmake -B build/ -DCMAKE_BUILD_TYPE=Debug
-cd build && make
+#!/bin/bash
+
+BUILD_DIR="build"
+BUILD_TYPE="Debug"
+RUN_AFTER_BUILD=0
+RUN_TESTS=0
+CLEAN=0
+
+case "$1" in
+  release)
+    BUILD_TYPE="Release"
+    ;;
+  drun)
+    BUILD_TYPE="Debug"
+    RUN_AFTER_BUILD=1
+    ;;
+  test)
+    BUILD_TYPE="Debug"
+    RUN_TESTS=1
+    ;;
+  clean)
+    CLEAN=1
+    ;;
+  help)
+    echo "Usage: ./build.sh [release | drun | test | clean]"
+    ;;
+esac
+
+if [ "$CLEAN" -eq 1 ]; then
+  echo "Cleaning build directory..."
+  rm -rf $BUILD_DIR
+  exit 0
+fi
+
+echo "Building in $BUILD_TYPE mode..."
+cmake -B $BUILD_DIR -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_MODULE_PATH=/usr/share/doc/check/examples/cmake
+cd $BUILD_DIR && make
+
+if [ "$RUN_AFTER_BUILD" -eq 1 ]; then
+  ./jugad-cli
+fi
+
+if [ "$RUN_TESTS" -eq 1 ]; then
+  ./tests
+fi
