86f1f61e703fef50c3b3b9cd6dc471b8487370fa
ItsSitanshu <sitanshu15shrestha@gmail.com>
2025-03-23 17:55:03 +0545

    Reconfigure project to only include C (pure jugadbase library code)

--- app/docs/[category]/[subpage]/[subsubpage]/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import DocsComponent from '@/app/ui/DocsComponent';
-
-const DocsSubsubpage = ({ params }: { params: { category: string; subpage: string; subsubpage: string } }) => {
-  return <DocsComponent page={params.category} subpage={params.subpage} subsubpage={params.subsubpage} />;
-};
-
-export default DocsSubsubpage;
--- app/docs/[category]/[subpage]/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import DocsComponent from '@/app/ui/DocsComponent';
-
-const DocsSubpage = ({ params }: { params: { category: string; subpage: string } }) => {
-  return <DocsComponent page={params.category} subpage={params.subpage} />;
-};
-
-export default DocsSubpage;
