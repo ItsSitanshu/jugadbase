0e09eec7018433989eabfcdd3368afe72f6bd9f3
ItsSitanshu <sitanshu15shrestha@gmail.com>
2025-03-27 15:29:09 +0545

    Add functionality for void* keys (dynamic value keys)

--- src/db/io.c
+++ src/db/io.c
@@ -128,12 +128,11 @@ int io_read_record(IO* io, int key, void* buffer, size_t record_size) {
   return 0;
 }
 
-int key_compare(void* key1, void* key2) {
-  return strcmp((char*)key1, (char*)key2);
-}
-
 BTree* btree_create() {
   BTree* tree = (BTree*)malloc(sizeof(BTree));
+
+  tree->btree_order = calculate_btree_order();
+
   tree->root = NULL;
   return tree;
 }
@@ -143,7 +142,6 @@ BTreeNode* btree_create_node(bool is_leaf) {
   node->is_leaf = is_leaf;
   node->num_keys = 0;
   memset(node->keys, 0, sizeof(node->keys));
-  memset(node->row_pointers, 0, sizeof(node->row_pointers));
   memset(node->children, 0, sizeof(node->children));
   return node;
 }
@@ -154,11 +152,12 @@ long btree_search(BTree* tree, void* key) {
   BTreeNode* node = tree->root;
   while (node) {
     int i = 0;
-    while (i < node->num_keys && key_compare(key, node->keys[i]) > 0) {
+
+    while (i < node->num_keys && compare_keys(key, node->keys[i], tree->key_type) > 0) {
       i++;
     }
 
-    if (i < node->num_keys && key_compare(key, node->keys[i]) == 0) {
+    if (i < node->num_keys && compare_keys(key, node->keys[i], tree->key_type) == 0) {
       return node->row_pointers[i]; 
     }
 
@@ -168,11 +167,13 @@ long btree_search(BTree* tree, void* key) {
   return -1;
 }
 
-void btree_split_child(BTreeNode* parent, int index, BTreeNode* child) {
-  int mid = BTREE_ORDER / 2;
+
+void btree_split_child(BTreeNode* parent, int index, BTreeNode* child, size_t btree_order) {
+  int mid = btree_order / 2;
   BTreeNode* new_node = btree_create_node(child->is_leaf);
 
   new_node->num_keys = mid - 1;
+
   for (int i = 0; i < mid - 1; i++) {
     new_node->keys[i] = child->keys[i + mid];
     new_node->row_pointers[i] = child->row_pointers[i + mid];
@@ -195,41 +196,45 @@ void btree_split_child(BTreeNode* parent, int index, BTreeNode* child) {
     parent->keys[i + 1] = parent->keys[i];
     parent->row_pointers[i + 1] = parent->row_pointers[i];
   }
-  
+
   parent->keys[index] = child->keys[mid - 1];
   parent->row_pointers[index] = child->row_pointers[mid - 1];
   parent->num_keys++;
 }
 
-void btree_insert_nonfull(BTreeNode* node, void* key, long row_offset) {
+void btree_insert_nonfull(BTree* tree, BTreeNode* node, void* key, long row_offset) {
   int i = node->num_keys - 1;
 
   if (node->is_leaf) {
-    while (i >= 0 && key_compare(key, node->keys[i]) < 0) {
+    while (i >= 0 && compare_keys(key, node->keys[i], tree->key_type) < 0) {
       node->keys[i + 1] = node->keys[i];
       node->row_pointers[i + 1] = node->row_pointers[i];
       i--;
     }
 
-    node->keys[i + 1] = strdup((char*)key);
+    void* new_key = malloc(sizeof_key(tree->key_type));
+    copy_key(new_key, key, tree->key_type);  
+
+    node->keys[i + 1] = new_key;
     node->row_pointers[i + 1] = row_offset;
     node->num_keys++;
   } else {
-    while (i >= 0 && key_compare(key, node->keys[i]) < 0) {
+    while (i >= 0 && compare_keys(key, node->keys[i], tree->key_type) < 0) {
       i--;
     }
 
-    if (node->children[i + 1]->num_keys == BTREE_ORDER - 1) {
-      btree_split_child(node, i + 1, node->children[i + 1]);
+    if (node->children[i + 1]->num_keys == tree->btree_order - 1) {
+      btree_split_child(node, i + 1, node->children[i + 1], tree->btree_order);
 
-      if (key_compare(key, node->keys[i + 1]) > 0) {
+      if (compare_keys(key, node->keys[i + 1], tree->key_type) > 0) {
         i++;
       }
     }
 
-    btree_insert_nonfull(node->children[i + 1], key, row_offset);
+    btree_insert_nonfull(tree, node->children[i + 1], key, row_offset);
   }
 }
+
 
 void btree_insert(BTree* tree, void* key, long row_offset) {
   if (!tree->root) {
@@ -237,14 +242,14 @@ void btree_insert(BTree* tree, void* key, long row_offset) {
   }
 
   BTreeNode* root = tree->root;
-  if (root->num_keys == BTREE_ORDER - 1) {
+  if (root->num_keys == tree->btree_order - 1) {
     BTreeNode* new_root = btree_create_node(false);
     new_root->children[0] = root;
-    btree_split_child(new_root, 0, root);
+    btree_split_child(new_root, 0, root, tree->btree_order);
     tree->root = new_root;
-    btree_insert_nonfull(new_root, key, row_offset);
+    btree_insert_nonfull(tree, new_root, key, row_offset);
   } else {
-    btree_insert_nonfull(root, key, row_offset);
+    btree_insert_nonfull(tree, root, key, row_offset);
   }
 }
 
@@ -264,4 +269,45 @@ void btree_destroy(BTree* tree) {
   if (!tree) return;
   btree_free_node(tree->root);
   free(tree);
+}
+
+int key_compare(void* key1, void* key2) {
+  return strcmp((char*)key1, (char*)key2);
+}
+
+int compare_keys(void* key1, void* key2, u_int8_t type) {
+  return strcmp((char*)key1, (char*)key2);
+}
+
+
+size_t sizeof_key(u_int32_t type) {
+  return 0;
+}
+
+void copy_key(void* dest, void* src, u_int32_t type) {
+  
+}
+
+long calculate_btree_order() {
+  size_t key_size = sizeof(int);               
+  size_t row_pointer_size = sizeof(long);     
+  size_t child_pointer_size = sizeof(BTreeNode*); 
+  
+  struct statfs fs_info;
+  if (statfs("/", &fs_info) == -1) {
+    return 4096;
+  }
+  unsigned long long block_size = fs_info.f_bsize; 
+
+  size_t node_size = (key_size + row_pointer_size + child_pointer_size);
+  
+  size_t total_size = node_size + sizeof(bool) + sizeof(int); 
+
+  unsigned long order = block_size / total_size;
+
+  if (order > MAX_KEYS_PER_NODE) {
+    order = MAX_KEYS_PER_NODE;
+  }
+
+  return order;
 }
--- src/db/io.h
+++ src/db/io.h
@@ -5,10 +5,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#ifdef _WIN32
+#include <windows.h>
+#include <stdio.h>
+
+#define statfs(path, buf) GetDiskFreeSpaceEx(path, NULL, NULL, (PULARGE_INTEGER)buf)
+
+struct statfs {
+  unsigned long long f_bsize;
+};
+#else
+#include <sys/statfs.h>
+#include <sys/types.h>
+#include <unistd.h>
+#endif
 
 #define MAX_TABLE_NAME 32
 #define MAX_SCHEMA 128
-#define BTREE_ORDER 4
+#define MAX_KEYS_PER_NODE 1000
 #define TABLE_COUNT_OFFSET 4UL
 
 typedef enum { IO_READ, IO_WRITE, IO_APPEND } IOMode;
@@ -29,13 +43,16 @@ typedef struct {
 typedef struct BTreeNode {
   bool is_leaf;
   int num_keys;
-  void* keys[BTREE_ORDER - 1];  
-  long row_pointers[BTREE_ORDER - 1];
-  struct BTreeNode* children[BTREE_ORDER]; 
+  void** keys;  
+  long* row_pointers;
+  struct BTreeNode** children; 
 } BTreeNode;
 
 typedef struct {
   BTreeNode* root;
+  long btree_order;
+  int key_type;
+  int* unique_key_types;
 } BTree;
 
 BTree* btree_create();
@@ -49,20 +66,25 @@ void io_seek(IO* io, long offset, int whence);
 void io_seek_write(IO* io, long offset, const void* data, size_t size, int whence);
 long io_tell(IO* io);
 
-void io_write_metadata(IO* io, const char* table_name, const char* schema);
 TableMetadata* io_read_metadata(IO* io, const char* table_name);
 
 int io_write_record(IO* io, int key, void* record, size_t record_size);
 int io_read_record(IO* io, int key, void* buffer, size_t record_size);
 
-int key_compare(void* key1, void* key2);
 BTree* btree_create();
 BTreeNode* btree_create_node(bool is_leaf);
 long btree_search(BTree* tree, void* key);
 void btree_insert(BTree* tree, void* key, long row_offset);
-void btree_insert_nonfull(BTreeNode* node, void* key, long row_offset);
-void btree_split_child(BTreeNode* parent, int index, BTreeNode* child);
+void btree_insert_nonfull(BTree* tree, BTreeNode* node, void* key, long row_offset);
+void btree_split_child(BTreeNode* parent, int index, BTreeNode* child, size_t btree_order);
 void btree_destroy(BTree* tree);
 void btree_free_node(BTreeNode* node);
+
+size_t sizeof_key(u_int32_t type);
+void copy_key(void* dest, void* src, u_int32_t type);
+int key_compare(void* key1, void* key2);
+int compare_keys(void* key1, void* key2, u_int8_t type);
+
+long calculate_btree_order();
 
 #endif // IO_H
