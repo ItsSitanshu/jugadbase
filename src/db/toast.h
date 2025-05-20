e7468f9a1a3cbb9edd3d85b3ae87f9a4c64b48c1
ItsSitanshu <sitanshu15shrestha@gmail.com>
2025-05-20 17:39:05 +0545

    Finish TOAST migration from respective databases => cluster specefic jb_core

--- src/db/toast.h
+++ src/db/toast.h
@@ -15,7 +15,6 @@ typedef struct {
 
 typedef struct Database Database;
 
-bool toast_create(Database* db);
 uint32_t toast_new_entry(Database* db, const char* data);
 char* toast_concat(Database* db, uint32_t toast_id);
 bool toast_delete(Database* db, uint32_t toast_id);
